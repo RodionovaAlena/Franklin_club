3f0f1ea27a595f33bdec875f89991c458c7ed681
Rodionova Alena <rodionova.alena@kupibilet.ru>
2021-09-23 10:51:57 +0300

    task corrected again

--- Homework3/task9.js
+++ Homework3/task9.js
@@ -1,18 +1,17 @@
-let a = 4;
-let b = 4;
-let c = 6;
+let a = 6;
+let b = 6;
+let c = 0;
 
 if (a<=b && a>=c || a>=b && a<=c) {
     let average = a;
     console.log(average);
-}
-
-if (b<=a && b>=c || b>=a && b<=c) {
+} else if (b<=a && b>=c || b>=a && b<=c) {
     let average = b;
     console.log(average);
-}
-
-if (c<=a && c>=b || c>=a && c<=b) {
+} else if (c<=a && c>=b || c>=a && c<=b) {
     let average = c;
     console.log(average);
-}
+} else if (a==b && a==c && b==c) {
+    let average = a;
+    console.log(average);
+}    
